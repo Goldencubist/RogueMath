f39bdcf6ffc062e00e02f126504a3ad0659e78aa
Golden Dev <miguizin.10@gmail.com>
2026-02-05 20:18:03 -0300

    Updates pré-desenvolvimento

--- RogueMath.py
+++ RogueMath.py
@@ -4,9 +4,9 @@ import os
 import pygame
 from golden_utils import true, false, none, rjson, wjson
 
-from states import state_sys, gs
-import UI.fonts
-from kerneldefs import initialization, GET_OUT
+from src.utils.states import state_sys, gs
+from src.fonts import fonts
+from src.utils.kerneldefs import initialization, GET_OUT
 
 sys.stdout = open(os.path.join(".RogueMath_data", "log"), "w", encoding="utf-8")
 sys.stderr = open(os.path.join(".RogueMath_data", "error_log"), "w", encoding="utf-8")
